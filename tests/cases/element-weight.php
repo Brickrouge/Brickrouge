987949eb9b59022855c70255a67131cbe0bec9c7
Olivier Laviale <contact@weirdog.com>
2014-11-19 15:59:02 +0100

    Added test for elements weight

---
+++ tests/cases/element-weight.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Brickrouge;
+
+echo new Element('div', [
+
+	Element::CHILDREN => [
+
+		'two' => new Text([ Element::WEIGHT => 1 ]),
+		'one' => new Text([ Element::WEIGHT => 0 ]),
+		'four' => new Text([ Element::WEIGHT => 'bottom' ]),
+		'three' => new Text([ Element::WEIGHT => 'before:four' ]),
+		'zero' => new Text([ Element::WEIGHT => 'top' ]),
+
+	]
+
+]);
