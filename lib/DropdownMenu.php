fc8534cafad93c7460e908a84d757e2c5704aeaa
Olivier Laviale <olivier.laviale@gmail.com>
2016-02-13 13:04:51 +0100

    Updated DropdownMenu for Bootstrap v4

--- lib/DropdownMenu.php
+++ lib/DropdownMenu.php
@@ -18,50 +18,86 @@ class DropdownMenu extends Element
 {
 	public function __construct(array $attributes = [])
 	{
-		parent::__construct('ul', $attributes);
+		parent::__construct('div', $attributes);
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	protected function render_inner_html()
 	{
 		$html = '';
 		$options = $this[self::OPTIONS];
-		$value = $this['value'];
+		$selected = $this['value'];
 
-		if ($value === null)
+		if ($selected === null)
 		{
-			$value = $this[self::DEFAULT_VALUE];
+			$selected = $this[self::DEFAULT_VALUE];
 		}
 
-		foreach ($options as $key => $option)
+		foreach ($options as $key => $item)
 		{
-			if ($option === false)
+			if ($item === false)
 			{
-				$html .= '<li class="divider"></li>';
+				$html .= $this->render_dropdown_divider();
 
 				continue;
 			}
-			else if ($option === null)
+			else if ($item === null)
 			{
 				continue;
 			}
 
-			$html .= '<li' . ((string) $key === (string) $value ? ' class="dropdown-item active"' : ' class="dropdown-item"') . '>';
-
-			if ($option instanceof Element)
-			{
-				$html .= $option;
-			}
-			else
-			{
-				$html .= '<a href="' . escape($key) . '" data-key="' . escape($key) . '">' . (is_string($option) ? escape($option) : $option) . '</a>';
-			}
-
-			$html .= '</li>';
+			$html .= $this->render_dropdown_item($key, $item, $selected);
 		}
 
 		return $html;
 	}
 
+	/**
+	 * Renders dropdown item.
+	 *
+	 * @param string|int $key
+	 * @param Element|string $item
+	 * @param mixed $selected
+	 *
+	 * @return A|Element
+	 */
+	protected function render_dropdown_item($key, $item, $selected)
+	{
+		if (!$item instanceof Element)
+		{
+			$item = new A($item, is_numeric($key) ? '#' : $key);
+		}
+
+		$item['data-key'] = $key;
+		$item->add_class('dropdown-item');
+
+		if ((string) $key === (string) $selected)
+		{
+			$item->add_class('active');
+		}
+
+		return $item;
+	}
+
+	/**
+	 * Renders dropdown divider.
+	 *
+	 * @return string
+	 */
+	protected function render_dropdown_divider()
+	{
+		return <<<EOT
+<div class="dropdown-divider"></div>
+EOT;
+	}
+
+	/**
+	 * Adds the `dropdown-menu` class.
+	 *
+	 * @inheritdoc
+	 */
 	protected function render_class(array $class_names)
 	{
 		return parent::render_class($class_names + [ 'dropdown-menu' => true ]);
