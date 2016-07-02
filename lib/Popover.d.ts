de2f7a16f8afd5ed3682c704a40224e33e24bcee
Olivier Laviale <olivier.laviale@gmail.com>
2016-07-02 15:53:59 +0200

    Added TypeScript declaration for Popover

---
+++ lib/Popover.d.ts
@@ -0,0 +1,44 @@
+declare namespace Brickrouge {
+
+    class Popover extends Subject {
+        static DEFAULT_OPTIONS: Popover.Options
+        static ActionEvent: Popover.ActionEvent
+        static from(options: Popover.FromOptions)
+
+        constructor(element: Element, options: Popover.Options)
+        attachAnchor(anchor: Element|string)
+        show()
+        hide()
+        isVisible(): boolean
+        reposition(quick: boolean)
+        observeAction(callback: Function)
+    }
+
+    namespace Popover {
+
+        interface Options {
+            anchor: Element|null,
+            animate: boolean,
+            popoverClass: string|null,
+            placement: string|null,
+            visible: boolean,
+            fitContent: boolean,
+            loveContent: boolean,
+            iframe: Element|null
+        }
+
+        interface FromOptions extends Options {
+            title: string
+            content: string|Element
+            actions: string|Array
+        }
+
+        interface ActionEvent {
+            action: string
+            popover: Popover,
+            event: Event
+        }
+
+    }
+
+}
